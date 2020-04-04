40681497f6d9282652bb8696ec851af29aede509
Ronald <ronald.lou88@gmail.com>
2020-04-04 10:40:01 -0500

    Add: auth middleware

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
   // http:localhost:5000/api/values
+  [Authorize]
   [Route("api/[controller]")]
   [ApiController]
 
@@ -29,7 +31,7 @@ namespace DatingApp.API.Controllers
       return Ok(values);
     }
 
-    // GET api/values/5
+    [AllowAnonymous]
     [HttpGet("{id}")]
     public async Task<IActionResult> GetValue(int id)
     {
