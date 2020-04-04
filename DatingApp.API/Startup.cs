6a6ef4e86ea83ea12d29286d0b64220ef21a9a1f
Ronald <ronald.lou88@gmail.com>
2020-04-03 22:33:55 -0500

    Updated commit

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -28,9 +28,9 @@ namespace DatingApp.API
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-      // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
       services.AddControllers();
       services.AddCors();
+      services.AddScoped<IAuthRepository, AuthRepository>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
