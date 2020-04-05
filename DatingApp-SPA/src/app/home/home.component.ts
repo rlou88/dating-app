765e54e6a7b37c6f90f241c19e53826c681bfa61
Ronald <ronald.lou88@gmail.com>
2020-04-04 20:25:04 -0500

    Add: register method to auth service

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   registerMode = false;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
 
   registerToggle() {
-    this.registerMode = !this.registerMode;
+    this.registerMode = true;
+  }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
   }
 }
--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -22,7 +22,8 @@ namespace DatingApp.API.Controllers
       _context = context;
 
     }
-    // GET api/values
+
+    [AllowAnonymous]
     [HttpGet]
     public async Task<IActionResult> GetValues()
     {
