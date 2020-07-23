1066d2da7ea646c933a15a6ec48c90e7ed8bdd8b
Ronald <ronald.lou88@gmail.com>
2020-07-23 16:43:09 -0500

    add: read logic

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -26,8 +26,21 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages() {
+    const currentUserId = +this.authService.decodedToken.nameid;
     this.userService
       .getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
+      .pipe(
+        tap((messages) => {
+          for (let i = 0; i < messages.length; i++) {
+            if (
+              messages[i].isRead === false &&
+              messages[i].recipientId === currentUserId
+            ) {
+              this.userService.markAsRead(currentUserId, messages[i].id);
+            }
+          }
+        }),
+      )
       .subscribe(
         (messages) => {
           this.messages = messages;
