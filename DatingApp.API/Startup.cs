cf13e694c062608ccd43e68dcf2b660e63b04648
Ronald <ronald.lou88@gmail.com>
2020-07-23 17:49:31 -0500

    add: preparing for deployment, wwwroot

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -23,83 +23,86 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-      services.AddControllers().AddNewtonsoftJson(opt =>
-      {
-        opt.SerializerSettings.ReferenceLoopHandling =
-          Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-      }
-      );
-      services.AddCors();
-      services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
-      services.AddAutoMapper(typeof(DatingRepository).Assembly);
-      services.AddScoped<IAuthRepository, AuthRepository>();
-      services.AddScoped<IDatingRepository, DatingRepository>();
-      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-        .AddJwtBearer(options =>
+        public Startup(IConfiguration configuration)
         {
-          options.TokenValidationParameters = new TokenValidationParameters
-          {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
-            ValidateIssuer = false,
-            ValidateAudience = false
-          };
-        });
-      services.AddScoped<LogUserActivity>();
-    }
+            Configuration = configuration;
+        }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-      else
-      {
-        app.UseExceptionHandler(builder =>
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
         {
-          builder.Run(async context =>
-          {
-            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-
-            var error = context.Features.Get<IExceptionHandlerFeature>();
-            if (error != null)
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers().AddNewtonsoftJson(opt =>
             {
-              context.Response.AddApplicationError(error.Error.Message);
-              await context.Response.WriteAsync(error.Error.Message);
+                opt.SerializerSettings.ReferenceLoopHandling =
+            Newtonsoft.Json.ReferenceLoopHandling.Ignore;
             }
-          });
-        });
-      }
+            );
+            services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
+            services.AddAutoMapper(typeof(DatingRepository).Assembly);
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(options =>
+              {
+                  options.TokenValidationParameters = new TokenValidationParameters
+                  {
+                      ValidateIssuerSigningKey = true,
+                      IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                      ValidateIssuer = false,
+                      ValidateAudience = false
+                  };
+              });
+            services.AddScoped<LogUserActivity>();
+        }
 
-      // app.UseHttpsRedirection();
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(builder =>
+                {
+                    builder.Run(async context =>
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-      app.UseRouting();
+                var error = context.Features.Get<IExceptionHandlerFeature>();
+                if (error != null)
+                {
+                    context.Response.AddApplicationError(error.Error.Message);
+                    await context.Response.WriteAsync(error.Error.Message);
+                }
+            });
+                });
+            }
 
-      app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            // app.UseHttpsRedirection();
 
-      app.UseAuthentication();
-      app.UseAuthorization();
+            app.UseRouting();
 
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-        // endpoints.MapFallbackToController("Index", "Fallback");
-      });
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapFallbackToController("Index", "Fallback");
+            });
+        }
     }
-  }
 }
